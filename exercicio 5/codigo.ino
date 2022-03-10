79dcb99776e797c9f63fb39c379526a6d4981e7e
Claudino Luany <99337058+claudinoLuany@users.noreply.github.com>
2022-03-09 23:35:56 -0300

    Create codigo.ino

---
+++ exercicio 5/codigo.ino
@@ -0,0 +1,28 @@
+const int botao1 = 4;
+const int led1 = 3;
+
+void setup()
+{
+  pinMode(botao1, INPUT);
+ 
+
+  pinMode(led1, OUTPUT);
+
+}
+
+int valor1;
+
+
+void loop()
+{
+  valor1 = digitalRead(botao1);
+  digitalWrite(led1, HIGH);
+ 
+ if(valor1){
+  digitalWrite(led1, LOW);
+  delay(10);
+
+ 
+ }
+  
+}
